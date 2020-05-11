074c4343d866d7e9c7e70cce6e6d6becb0ae9ec9
Simon Fernandez <simon.fernandez@ens-lyon.fr>
2020-05-11 16:15:03 +0200

    first ODNS proxy to DoH

---
+++ intercept.py
@@ -0,0 +1,153 @@
+import socket
+import json
+
+from dnslib import RCODE, QTYPE, RR
+from dnslib.server import DNSServer, DNSHandler, BaseResolver, DNSLogger
+from odns import ODNSCypher
+from urllib.request import Request, urlopen
+
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+ODNS_SUFFIX = ".odns"
+DOH_SERVER = "cloudflare-dns.com"
+
+
+class ODNSLocalProxy(BaseResolver):
+
+    """
+        ODNS Local Intercepting resolver
+
+        Cyphers all requests received and forwards them to a recursive resolver
+    """
+
+    def __init__(self, upstream, skip):
+        """
+            upstream        - upstream DoH server
+            skip            - list of wildcard labels to skip
+        """
+        self.upstream = upstream
+        self.skip = skip
+        self.crypto = ODNSCypher()
+        self.server_pk = None
+
+    def resolve(self, request, handler):
+        reply = request.reply()
+        qname = request.q.qname
+        qtype = request.q.qtype
+        logging.debug("QNAME : ", qname)
+
+        # Test if we skip the request
+        if not any([qname.matchGlob(s) for s in self.skip]):
+            # Cypher the query
+            query = bytes(qname, encoding="utf-8")
+            encrypted, aes_key = self.crypto.encrypt_query(
+                query, self.server_pk)
+
+            new_qname = encrypted.hex() + ODNS_SUFFIX
+            logging.debug("Encrypted : ", new_qname)
+
+            # Forward it to the upstream server
+            try:
+                json = query(new_qname, type=qtype, server=self.upstream)
+                for entry in json["Answer"]:
+                    # Decypher the reply
+                    answer = self.crypto.decrypt_answer(entry["data"], aes_key)
+                    reply.add_answer(
+                        RR(entry["name"], QTYPE[entry["type"]], rdata=answer))
+                    logging.debug(f"{qname} -> {answer}")
+            except Exception as ex:
+                logging.debug(f"Problem in the DOH resolution : {ex}")
+                reply.header.rcode = getattr(RCODE, 'NXDOMAIN')
+
+        # Else, just forward
+        if not reply.rr:
+            logging.debug("Query forwarded")
+            try:
+                json = query(new_qname, type=qtype, server=self.upstream)
+                for entry in json["Answer"]:
+                    # Decypher the reply
+                    reply.add_answer(
+                        RR(entry["name"], QTYPE[entry["type"]], rdata=entry["data"]))
+            except socket.timeout:
+                reply.header.rcode = getattr(RCODE, 'NXDOMAIN')
+
+        return reply
+
+    def query(name, type='A', server=DOH_SERVER, path="/dns-query"):
+        """
+        Queries the server through the JSON interface
+        Ref : https://developers.cloudflare.com/1.1.1.1/dns-over-https/json-format/
+        """
+
+        # Send the request
+        addr = f"https://{server}{path}?name={name}&type={type}"
+        req = Request(addr, headers={"Accept": "application/dns-json"})
+        # Parse the answer
+        content = urlopen(req).read().decode()
+        return json.loads(content)
+
+
+if __name__ == '__main__':
+
+    import argparse
+    import time
+
+    p = argparse.ArgumentParser(description="ODNS Local Proxy")
+    p.add_argument("--port", "-p", type=int, default=53,
+                   metavar="<port>",
+                   help="Local proxy port (default:53)")
+    p.add_argument("--address", "-a", default="",
+                   metavar="<address>",
+                   help="Local proxy listen address (default:all)")
+    p.add_argument("--upstream", "-u", default=DOH_SERVER,
+                   metavar="<dns server:port>",
+                   help=f"Upstream DoH server (default:{DOH_SERVER})")
+    p.add_argument("--skip", "-s", action="append",
+                   metavar="<label>",
+                   help="Don't intercept matching label (glob)")
+    p.add_argument("--log", default="request,reply,truncated,error",
+                   help="Log hooks to enable (default: +request,+reply,+truncated,+error,-recv,-send,-data)")
+    p.add_argument("--log-prefix", action='store_true', default=False,
+                   help="Log prefix (timestamp/handler/resolver) (default: False)")
+    args = p.parse_args()
+
+    args.dns, _, args.dns_port = args.upstream.partition(':')
+    args.dns_port = int(args.dns_port or 53)
+
+    resolver = ODNSLocalProxy(args.upstream,
+                              args.skip or [])
+    logger = DNSLogger(args.log, args.log_prefix)
+
+    print("Starting ODNS Local Poxy (%s:%d -> %s)" % (
+        args.address or "*", args.port,
+        args.upstream))
+
+    if resolver.skip:
+        print("    Skipping:", ", ".join(resolver.skip))
+    print()
+
+    DNSHandler.log = {
+        'log_request',      # DNS Request
+        'log_reply',        # DNS Response
+        'log_truncated',    # Truncated
+        'log_error',        # Decoding error
+    }
+
+    udp_server = DNSServer(resolver,
+                           port=args.port,
+                           address=args.address,
+                           logger=logger)
+    udp_server.start_thread()
+
+    if args.tcp:
+        tcp_server = DNSServer(resolver,
+                               port=args.port,
+                               address=args.address,
+                               tcp=True,
+                               logger=logger)
+        tcp_server.start_thread()
+
+    while udp_server.isAlive():
+        time.sleep(1)
---
+++ odns.py
@@ -0,0 +1,69 @@
+from typing import Union
+
+from coincurve import PrivateKey, PublicKey
+from ecies.utils import generate_key, hex2prv, hex2pub, encapsulate, decapsulate, aes_encrypt, aes_decrypt
+
+
+class ODNSCypher():
+    """
+        Holding all cryptography algorithms to encrypt and decrypt queries and
+        answers.
+    """
+
+    def __init__(self):
+        pass
+
+    def encrypt_query(query: bytes, server_pk: Union[str, bytes]):
+        """
+            Encrypt the query to send to the server
+
+            query     - The query
+            server_pk - The public key of the server
+        """
+        # Generate the ephemeral key
+        ephemeral_key = generate_key()
+
+        # Parse the server key
+        if isinstance(server_pk, str):
+            server_pubkey = hex2pub(server_pk)
+        elif isinstance(server_pk, bytes):
+            server_pubkey = PublicKey(server_pk)
+        else:
+            raise TypeError("Invalid public key type")
+
+        # Generate the symetric key
+        aes_key = encapsulate(ephemeral_key, server_pubkey)
+        # Encrypt the query (this adds the nonce)
+        cipher_text = aes_encrypt(aes_key, query)
+        # Return the message and the symetric key
+        return ephemeral_key.public_key.format(False) + cipher_text, aes_key
+
+    def decrypt_query(query: bytes, server_sk: Union[str, bytes]):
+        """
+            Decrypts a query received by the server
+
+            query - The query to decrypt
+            server_sk - The server private key
+        """
+        # Parse the server key
+        if isinstance(server_sk, str):
+            private_key = hex2prv(server_sk)
+        elif isinstance(server_sk, bytes):
+            private_key = PrivateKey(server_sk)
+        else:
+            raise TypeError("Invalid secret key type")
+
+        # Parsre the msg, extract the pubkey and the {IV || payload} from the query
+        pubkey = query[0:65]  # uncompressed pubkey's length is 65 bytes
+        encrypted = query[65:]
+        ephemeral_public_key = PublicKey(pubkey)
+
+        # Generate the AES key
+        aes_key = decapsulate(ephemeral_public_key, private_key)
+        return aes_decrypt(aes_key, encrypted), aes_key
+
+    def encrypt_answer(answer: bytes, aes_key: bytes):
+        return aes_encrypt(aes_key, answer)
+
+    def decrypt_answer(answer: bytes, aes_key: bytes):
+        return aes_decrypt(aes_key, answer)
